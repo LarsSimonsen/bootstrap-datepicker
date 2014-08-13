c712e2decf2a795b63361c7a708f6a31bb6cfae5
LarsSimonsen <LarsSimonsen@users.noreply.github.com>
2014-08-13 11:34:33 +0200

    Create bootstrap-datepicker.nn.js

---
+++ bootstrap-datepicker.nn.js
@@ -0,0 +1,16 @@
+/**
+ *  Norwegian (nynorsk) translation for bootstrap-datepicker
+ **/
+;(function($){
+  $.fn.datepicker.dates['no'] = {
+    days: ['Sundag','Måndag','Tysdag','Onsdag','Torsdag','Fredag','Laurdag'],
+    daysShort: ['Sun','Mån','Tir','Ons','Tor','Fre','Lau'],
+    daysMin: ['Su','Må','Ti','On','To','Fr','La'],
+    months: ['Januar','Februar','Mars','April','Mai','Juni','Juli','August','September','Oktober','November','Desember'],
+    monthsShort: ['Jan','Feb','Mar','Apr','Mai','Jun','Jul','Aug','Sep','Okt','Nov','Des'],
+    today: 'I dag',
+    clear: 'Still tilbake',
+    weekStart: 1,
+    format: 'dd.mm.yyyy'
+  };
+}(jQuery));
